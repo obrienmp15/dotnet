afee523afbff3fb0f1229e9478eb6a84d44cb1a2
matt obrien <obrien.matthew.cs@gmail.com>
2016-10-27 10:59:04 -0400

    add the insert data feature

---
+++ MonsterApp.DataAccess/MonsterApp.Tests/AdoDataInsertTests.cs
@@ -0,0 +1,30 @@
+﻿using MonsterApp.DataAccess;
+using MonsterApp.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MonsterApp.Tests
+{
+  public partial class AdoDataTests
+  {
+    private Gender gender;
+    public AdoDataTests()
+    {
+      gender = new Gender() { GenderName = "TestGender" };
+    }
+    [Fact]
+
+    public void Test_InsertGender()
+    {
+      var data = new AdoData();
+      var actual = data.InsertGender(gender);
+
+      Assert.True(actual);
+
+    }
+  }
+}
