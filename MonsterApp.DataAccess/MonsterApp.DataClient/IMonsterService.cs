93c90eb4182fe394806601505db72e8eaf62d3b0
matt obrien <obrien.matthew.cs@gmail.com>
2016-10-31 12:03:42 -0400

    Add the service project

---
+++ MonsterApp.DataAccess/MonsterApp.DataClient/IMonsterService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MonsterApp.DataClient
+{
+  /// <summary>
+  /// 
+  /// 
+  /// </summary>
+  [ServiceContract]
+  public interface IMonsterService
+  {
+    [OperationContract]
+    string DoWork();
+
+    //void Menu();
+
+  }
+}
